41d6f140749081fd2d61f087dfb8269e2aa04e59
Gota Okabe <go.sirasagi62@gmail.com>
2025-12-07 02:19:16 +0900

    Fix version info

--- cmd/root.go
+++ cmd/root.go
@@ -19,7 +19,7 @@ var (
 	// CommonIgnorePath は共通無視ファイルのパスです
 	CommonIgnorePath string
 	// Version は mushi のバージョンです
-	Version string = "v0.2.0"
+	Version string = "v0.2.2"
 )
 
 // Config は mushi の設定を保持する構造体です
